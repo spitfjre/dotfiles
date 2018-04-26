a9141b6a2c737e163fe69c66c2dbcf46c653245d
spitfjre <you@example.com>
2018-04-26 05:00:35 +0200

    readded tex stuff

--- install.sh
+++ install.sh
@@ -51,6 +51,11 @@ yaourt -S --noconfirm --needed jetbrains-toolbox
 yaourt -S --noconfirm --needed android-studio
 yaourt -S --noconfirm --needed sublime-text
 
+# tex
+yaourt -S --noconfirm --needed texmaker
+yaourt -S --noconfirm --needed texlive-most
+yaourt -S --noconfirm --needed texlive-lang
+
 # font
 yaourt -S --noconfirm --needed ttf-material-icons
 
