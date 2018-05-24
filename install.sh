550f2fba478dc8c1da2ef2dafb4750b035fbdd02
spitfjre <you@example.com>
2018-05-24 11:55:39 +0200

    added networkmanager to default installation

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ yaourt -S --noconfirm --needed lxappearance
 yaourt -S --noconfirm --needed numlockx
 yaourt -S --noconfirm --needed mpd
 yaourt -S --noconfirm --needed mpc
+yaourt -S --noconfirm --needed networkmanager
 yaourt -S --noconfirm --needed compton
 yaourt -S --noconfirm --needed polybar-git # Place polybar last to ensure polybar also installs the mpd module
 
