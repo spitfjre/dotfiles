30b16bbac9085d2c57c26cb2fcab9a6fb149a129
spitfjre <you@example.com>
2018-12-29 19:21:25 +0100

    Updated copy scripts.

--- copyFromRepository.sh
+++ copyFromRepository.sh
@@ -6,11 +6,11 @@ mkdir $HOME/Pictures/wallpaper
 mkdir $HOME/.ncmpcpp
 mkdir $HOME/.fonts
 
-cp -rv $repoDir/.config/* $HOME/.config/
-cp -rv $repoDir/wallpaper/* $HOME/Pictures/wallpaper/
-cp -rv $repoDir/.fonts/* $HOME/.fonts/
-cp $repoDir/.ncmpcpp/config $HOME/.ncmpcpp/
+cp -rv ${repoDir}/.config/* $HOME/.config/
+cp -rv ${repoDir}/wallpaper/* $HOME/Pictures/wallpaper/
+cp -rv ${repoDir}/.fonts/* $HOME/.fonts/
+cp ${repoDir}/.ncmpcpp/config $HOME/.ncmpcpp/
 
-cp $repoDir/.Xresources $HOME
-cp $repoDir/.zprofile $HOME
-cp $repoDir/.zshrc $HOME
+cp ${repoDir}/.Xresources $HOME
+cp ${repoDir}/.zprofile $HOME
+cp ${repoDir}/.zshrc $HOME
--- copyToRepository.sh
+++ copyToRepository.sh
@@ -2,28 +2,27 @@
 
 repoDir="$HOME/Workspace/dotfiles"
 
-cp -r $HOME/.config/alacritty/* $repoDir/.config/alacritty/
-cp -r $HOME/.config/dunst/* $repoDir/.config/dunst/
-cp -r $HOME/.config/htop/* $repoDir/.config/htop/
-cp -r $HOME/.config/mpd/mpd.conf $repoDir/.config/mpd/
-cp -r $HOME/.config/ranger/rc.conf $repoDir/.config/ranger/
-cp -r $HOME/.config/rofi/* $repoDir/.config/rofi/
-cp -r $HOME/.config/sway/* $repoDir/.config/sway/
-cp -r $HOME/.config/termite/* $repoDir/.config/termite/
-cp -r $HOME/.config/waybar/* $repoDir/.config/waybar/
+cp -r $HOME/.config/alacritty/* ${repoDir}/.config/alacritty/
+cp -r $HOME/.config/htop/* ${repoDir}/.config/htop/
+cp -r $HOME/.config/mpd/* ${repoDir}/.config/mpd/
+cp -r $HOME/.config/ranger/* ${repoDir}/.config/ranger/
+cp -r $HOME/.config/rofi/* ${repoDir}/.config/rofi/
+cp -r $HOME/.config/sway/* ${repoDir}/.config/sway/
+cp -r $HOME/.config/termite/* ${repoDir}/.config/termite/
+cp -r $HOME/.config/waybar/* ${repoDir}/.config/waybar/
 
-cp -r $HOME/Pictures/wallpaper/* $repoDir/wallpaper/
-cp -r $HOME/.fonts/* $repoDir/.fonts/
+cp -r $HOME/Pictures/wallpaper/* ${repoDir}/wallpaper/
+cp -r $HOME/.fonts/* ${repoDir}/.fonts/
 
-if [ -e $HOME/.Xresources ]
- then cp $HOME/.Xresources $repoDir/
+if [[ -e $HOME/.Xresources ]]
+ then cp $HOME/.Xresources ${repoDir}/
 fi
-if [ -e $HOME/.zprofile ]
- then cp $HOME/.zprofile $repoDir/
+if [[ -e $HOME/.zprofile ]]
+ then cp $HOME/.zprofile ${repoDir}/
 fi
-if [ -e $HOME/.zshrc ]
- then cp $HOME/.zshrc $repoDir/
+if [[ -e $HOME/.zshrc ]]
+ then cp $HOME/.zshrc ${repoDir}/
 fi
-if [ -e $HOME/.ncmpcpp/config ]
- then cp $HOME/.ncmpcpp/config $repoDir/.ncmpcpp/
+if [[ -e $HOME/.ncmpcpp/config ]]
+ then cp $HOME/.ncmpcpp/config ${repoDir}/.ncmpcpp/
 fi
