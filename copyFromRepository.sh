7c486c2c6a968646c33e221f854d1d0267916633
Maximilian Aulbach <maximilian.b.aulbach@stud-mail.uni-wuerzburg.de>
2020-10-05 18:02:55 +0200

    Updated/cleaned configs (#1)
    * Updated/cleaned configs.
    
    * Updated polybar title background color.

--- copyFromRepository.sh
+++ copyFromRepository.sh
@@ -3,13 +3,10 @@
 repoDir="$HOME/Workspace/dotfiles"
 
 mkdir $HOME/Pictures/wallpaper
-mkdir $HOME/.ncmpcpp
 
 cp -rv $repoDir/.config/* $HOME/.config/
 cp -rv $repoDir/wallpaper/* $HOME/Pictures/wallpaper/
-cp $repoDir/.ncmpcpp/config $HOME/.ncmpcpp/
 
-cp $repoDir/.xinitrc $HOME
 cp $repoDir/.Xresources $HOME
 cp $repoDir/.gitconfig $HOME
 cp $repoDir/.zprofile $HOME
--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
+sudo pacman -S --noconfirm --needed yay
+yay -S --noconfirm --needed base-devel
 yay -S --noconfirm --needed archlinux-keyring
-
-# xorg
-yay -S --noconfirm --needed xorg-server
-yay -S --noconfirm --needed xorg-apps
-yay -S --noconfirm --needed xorg
-yay -S --noconfirm --needed xorg-xinit
 
 # network manager
 yay -S --noconfirm --needed openvpn
-yay -S --noconfirm --needed networkmanager
+yay -S --noconfirm --needed openvpn-update-resolv-conf-git
 
 # i3
 yay -S --noconfirm --needed i3-gaps-next-git
@@ -20,10 +16,8 @@ yay -S --noconfirm --needed scrot
 yay -S --noconfirm --needed rofi
 yay -S --noconfirm --needed arandr
 yay -S --noconfirm --needed lxappearance
-yay -S --noconfirm --needed mpd
 yay -S --noconfirm --needed compton
-yay -S --noconfirm --needed mpc
-yay -S --noconfirm --needed polybar # Place polybar last to ensure polybar also installs the mpd module
+yay -S --noconfirm --needed polybar
 
 # gtk-theme
 yay -S --noconfirm --needed mojave-gtk-theme-git
@@ -31,20 +25,20 @@ yay -S --noconfirm --needed mojave-ct-icon-theme
 
 # terminal
 yay -S --noconfirm --needed alacritty
-yay -S --noconfirm --needed termite
 yay -S --noconfirm --needed htop
-yay -S --noconfirm --needed ncmpcpp
 yay -S --noconfirm --needed ranger
 
 # zsh
 yay -S --noconfirm --needed zsh
 yay -S --noconfirm --needed zsh-completions
-yay -S --noconfirm --needed oh-my-zsh-git
-yay -S --noconfirm --needed zsh-theme-powerlevel9k
+yay -S --noconfirm zsh-theme-powerlevel10k-git
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+chsh -s $(which zsh)
 
-# browser
+# browser & mail
 yay -S --noconfirm --needed firefox
 yay -S --noconfirm --needed google-chrome
+yay -S --noconfirm --needed thunderbird
 
 # ide & editor
 yay -S --noconfirm --needed jetbrains-toolbox
@@ -57,26 +51,32 @@ yay -S --noconfirm --needed ttf-font-awesome
 yay -S --noconfirm --needed system-san-francisco-font-git
 
 # chat
-yay -S --noconfirm --needed rambox-bin
+yay -S --noconfirm --needed slack-desktop
 yay -S --noconfirm --needed discord
 
 # dev
 yay -S --noconfirm --needed git
+yay -S --noconfirm --needed vim
+yay -S --noconfirm --needed postman
+
+## java
 yay -S --noconfirm --needed jdk8-openjdk
 yay -S --noconfirm --needed maven
+
+## node
 yay -S --noconfirm --needed nodejs
 yay -S --noconfirm --needed npm
-yay -S --noconfirm --needed vim
-yay -S --noconfirm --needed postman-bin
+yay -S --noconfirm --needed yarn
+
+### angular
+sudo npm i -g @angular/cli
 
 # misc
 yay -S --noconfirm --needed jdownloader2
-yay -S --noconfirm --needed keeweb
+yay -S --noconfirm --needed keepassxc
 yay -S --noconfirm --needed mpv
 yay -S --noconfirm --needed dropbox
-yay -S --noconfirm --needed thunderbird
-yay -S --noconfirm --needed gimp
-yay -S --noconfirm --needed evince
+yay -S --noconfirm --needed audacious
 
 # docker
 yay -S --noconfirm --needed docker
